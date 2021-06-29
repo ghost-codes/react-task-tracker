418537e3938ba1ddea5884afb163fdc28c37a1c4
hopedorkenoo <compounddork@gmail.com>
2021-06-29 14:33:15 +0000

    Task Delete Implemented

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,14 +1,47 @@
+import { useState } from 'react'
+import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
 
-  const name = 'Hope';
-  const x = true;
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false,
+    },
+
+
+  ]);
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
   return (
     <div className="container">
-      <h1>Hello From React</h1>
-      <h2>Hello {x ? 'Yes' : 'No'}</h2>
+      <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> :
+        <p>No Tasks To Display</p>
+      }
     </div>
   );
 }
+
+
 
 export default App;
--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,11 +1,24 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
 
-
-const Header = () => {
+const Header = ({ title }) => {
+    const onClick = (e) => {
+        console.log(e);
+    }
     return (
-        <div>
-
-        </div>
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick} />
+        </header>
     )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header
